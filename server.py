45a8ea6d5110794ee08d85a49a0f78e943885f44
Данила Филатов <yadanilfilatov@gmail.com>
2023-06-21 13:28:24 +0300

    Nickname choosing

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 from asyncio import StreamWriter, StreamReader
-from typing import Union
+from typing import Union, ForwardRef
 
 logging.basicConfig(
     level=logging.INFO,
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 
 class Message:
-    def __init__(self, message: str, sender):
+    def __init__(self, message: str, sender: ForwardRef('User')):
         self.__message = message
         self.__sender = sender
 
@@ -35,29 +35,48 @@ class User:
         self.__address = address
         self.__reader = reader
         self.__writer = writer
+        self.__nickname = ''
 
     @property
     def address(self):
         return self.__address
 
+    @property
+    def nickname(self):
+        return self.__nickname
+
+    @nickname.setter
+    def nickname(self, value):
+        self.__nickname = value
+
     async def send_message(self, msg: Union[Message, str]):
-        """Sending a message from user."""
+        """Sending a message to user."""
+        logger.info(f'Sent message \n'
+                    f'{str(msg)} \n'
+                    f'to {self}')
         self.__writer.write((str(msg) + '\n').encode('utf8'))
+        await self.__writer.drain()
 
     async def get_message(self) -> Message:
         """Getting a new message from user."""
         data = await self.__reader.read(1024)
-        return Message(message=data.decode()[:-1], sender=self)
+        message = data.decode()[:-1]
+        logger.info(f'Got a new message\n'
+                    f'{message}\n'
+                    f'from {self}')
+        return Message(message=message, sender=self)
 
     def __repr__(self) -> str:
-        return ':'.join(map(str, self.address))
+        return self.nickname
 
 
 class Server:
+    GROUP_MESSAGES_LIMIT = 20
+
     def __init__(self, host="127.0.0.1", port=8000):
         self.__host = host
         self.__port = port
-        self.__users = []
+        self.__users = dict()
         self.__history = []
 
     @property
@@ -68,21 +87,28 @@ class Server:
     def history(self):
         return self.__history
 
+    @history.setter
+    def history(self, value):
+        self.__history = value
+
     async def client_connected(self, reader: StreamReader,
                                writer: StreamWriter):
         """Handling a connected user."""
         address = writer.get_extra_info('peername')
         logger.info(f'New connection from {address}')
         new_user = User(address, reader, writer)
-        self.users.append(new_user)
-        await self._send_to_group_chat(
-            Message(sender=new_user,
-                    message='has been connected')
-        )
+        await new_user.send_message('Please type your nickname\n')
+        await self._set_nickname(new_user)
+        print(new_user)
+
+        connected_message = Message(sender=new_user,
+                                    message='has been connected')
+        await self._send_to_group_chat(connected_message)
+
+        print(connected_message)
 
         await self._send_chat_history(new_user)
         await self._listen(new_user)
-        print('Client connected')
 
     async def start_server(self):
         """Starting a server."""
@@ -94,22 +120,59 @@ class Server:
         async with srv:
             await srv.serve_forever()
 
-    async def _listen(self, user):
+    async def _listen(self, user: User) -> None:
         """Listening for user messages."""
         while True:
             msg = await user.get_message()
             logger.info(f'Got new message {msg.message} from {msg.sender}')
+
+            # if msg.message[0] == '/':
+            #     await self._parse_command(msg)
+            # else:
             await self._send_to_group_chat(msg)
 
-    async def _send_chat_history(self, user):
-        for msg in self.history:
-            await user.send_message(msg)
+    async def _send_chat_history(self, user: User) -> None:
+        await user.send_message(
+            'Group chat history:\n' +
+            '\n'.join(map(str, self.history)) +
+            '\n'
+        )
 
-    async def _send_to_group_chat(self, msg: Message):
-        self.history.append(msg)
-        for user in self.users:
-            if user != msg.sender:
-                await user.send_message(msg)
+    async def _send_to_group_chat(self, msg: Message) -> None:
+        if msg.message[:-1]:
+            self.history.append(msg)
+            self.history = self.history[:self.GROUP_MESSAGES_LIMIT]
+            for user in self.users.values():
+                if user != msg.sender:
+                    await user.send_message(msg)
+
+    def _parse_command(self, msg):
+        command, body = msg.message.split(maxsplit=1)
+        if command == '/private':
+            try:
+                nickname, text = body.split(maxsplit=1)
+            except Exception as e:
+                logger.error(e)
+                logger.error(e)
+                logger.error(e)
+
+    async def _set_nickname(self, user):
+        nickname = None
+        while not self._is_valid_nickname(nickname):
+            msg = await user.get_message()
+            nickname = msg.message[:-1]
+
+            await user.send_message('User with nickname is already in chat\n'
+                                    'Please choose another one')
+        user.nickname = nickname
+        self.users[nickname] = user
+        await user.send_message(f'Welcome here, {nickname}\n\n')
+
+    def _is_valid_nickname(self, nickname):
+        if nickname:
+            if nickname not in self.users:
+                return True
+        return False
 
 
 if __name__ == '__main__':
